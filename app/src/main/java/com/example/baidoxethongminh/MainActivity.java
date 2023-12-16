9b5f187db55d0716b61f2ca934658ebab60c401d
Lan Anh <Lan Anh>
2023-12-16 23:44:08 +0700

    codebaidoxe

--- app/src/main/java/com/example/baidoxethongminh/MainActivity.java
+++ app/src/main/java/com/example/baidoxethongminh/MainActivity.java
@@ -15,19 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        @SuppressLint("WrongViewCast") Button myButton = findViewById(R.id.battatden);
-
-        myButton.setOnClickListener(v -> {
-            // Xử lý khi Button được nhấn
-            // Đặt hành động mà bạn muốn thực hiện ở đây
-            doSomethingOnClick();
-        });
-    }
-
-    private void doSomethingOnClick() {
-        // Hành động cần thực hiện khi Button được nhấn
-        Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show();
-        // Thay đổi hoặc thực hiện hành động khác tùy theo nhu cầu của bạn
     }
 
 }
